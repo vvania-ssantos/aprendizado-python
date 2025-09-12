a72521480852d84c4739cc757b7eec9d7dba2ced
vvania-ssantos <supervaniaa@gmail.com>
2025-09-12 15:31:25 -0300

    Python: aprendendo a linguagem

---
+++ aprendizado-python/criando um programa que calcula o preço total de um produto.py
@@ -0,0 +1,33 @@
+print("Início do programa")
+produto = "Notebook" # texto
+preco_unitario = 3500.50 # float
+quantidade = 2 # inteiro
+total_compra = preco_unitario * quantidade 
+
+
+
+print(produto, preco_unitario, quantidade)
+print(type (produto))
+print(type(preco_unitario))
+print(type(quantidade))
+print(type (total_compra))
+
+print("Fim do Programa")
+
+# As variáveis que você já tem
+produto = "Notebook"
+preco_unitario = 3500.50
+quantidade = 2
+
+# A linha que você corrigiu
+total_compra = preco_unitario * quantidade
+
+# A linha que você precisa ajustar para exibir o valor
+print(total_compra)
+
+produto = "Notebook"
+preco_unitario = 3500.50
+quantidade = 2
+total_compra = preco_unitario * quantidade
+frase = f"O preço total da compra de 2 Notebooks é R$ 7001.0"
+print(frase)
